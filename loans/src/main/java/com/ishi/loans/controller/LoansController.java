3f7bad194ad4a7c54177a9b3690cf2594dcd83fc
AnjaniKrPathak <eranjanipathak.it@gmail.com>
2022-08-27 03:24:30 +0530

    Updated All Loan Method

--- loans/src/main/java/com/ishi/loans/controller/LoansController.java
+++ loans/src/main/java/com/ishi/loans/controller/LoansController.java
@@ -3,6 +3,9 @@ package com.ishi.loans.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LoansController {
     @GetMapping("/")
@@ -10,7 +13,8 @@ public class LoansController {
         return "Welcome to Ishi World Bank Loan Section !!!";
     }
     @GetMapping("/{userId}")
-    public void getAllLoans(){
+    public List<String> getAllLoans(){
+        return new ArrayList<>();
 
     }
 }
